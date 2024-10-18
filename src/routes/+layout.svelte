5f19a708534ba9ce35ebbc60910ea43aaa727ded
Nolankicks <nolandimensions@gmail.com>
2024-10-18 16:36:55 -0400

    Updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
+<script>
+    import "../app.css";
+    import Background from "../lib/components/Background.svelte";
+</script>
+
+<nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-transparentblack1 p-2 drop-shadow-md text-white">
+    <div class="container mx-auto flex flex-row items-center justify-between">
+        <a href="/" class="hover:scale-105 transition-all active:scale-95">kicks</a>
+
+        <div class="flex space-x-4">
+            <a href="/projects" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Projects</a>
+        </div>
+    </div>
 </nav>
+
+<Background />
 
 <slot></slot>
