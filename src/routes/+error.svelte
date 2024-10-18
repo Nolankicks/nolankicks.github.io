4b77d62ec7d1ed7ca0a5fc0cab49d8234801dc5b
Nolankicks <nolandimensions@gmail.com>
2024-10-18 17:45:21 -0400

    Clean up

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,6 @@
     <h1 class="text-4xl font-bold">Page Not Found</h1>
     <p class="text-lg">The page you are looking for does not exist.</p>
     <div class="text-blue-500">
-        Check out <a class="hover:underline text-blue" href="https://sbox.game/nolankicks">my games</a> 😏
+        Check out <a class="hover:underline text-blue" href="https://sbox.game/nolankicks">my games</a> 😉
     </div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,15 @@
     import Background from "../lib/components/Background.svelte";
 </script>
 
-<nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-transparentblack1 p-2 drop-shadow-md text-white font-poppins">
+<nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-black p-2 drop-shadow-md text-white font-poppins">
     <div class="container mx-auto flex flex-row items-center justify-between">
         <a href="/" class="hover:scale-105 transition-all active:scale-95">kicks</a>
-
+        
+        <!--
         <div class="flex space-x-4">
             <a href="/projects" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Projects</a>
         </div>
+        -->
     </div>
 </nav>
 
