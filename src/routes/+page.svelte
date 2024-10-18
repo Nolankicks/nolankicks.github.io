cab8f1031fdcceab629aad5dd8727b0b1c7b5d29
Nolankicks <nolandimensions@gmail.com>
2024-10-18 08:55:22 -0400

    New website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const script = document.createElement('script');
+        script.src = 'https://tenor.com/embed.js';
+        script.async = true;
+        document.body.appendChild(script);
+    });
+</script>
+
+<div>Kicks Collective</div>
+
+<div style="height: 200px; width: 200px; overflow: hidden;" class="tenor-gif-embed" data-postid="1762375008245577527" data-share-method="host" data-aspect-ratio="1" data-width="200px">
+    <a href="https://tenor.com/view/hawk-tuah-minion-spit-on-that-thang-gif-1762375008245577527">Hawk Tuah Minion GIF</a> from 
+    <a href="https://tenor.com/search/hawk+tuah-gifs">Hawk Tuah GIFs</a>
+</div>
