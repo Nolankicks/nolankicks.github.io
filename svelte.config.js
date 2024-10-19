1de49c1ec9bb4c868f0ec9d38751b827de9d26f9
Nolankicks <nolandimensions@gmail.com>
2024-10-18 21:04:17 -0400

    Test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: vitePreprocess(),
+
     kit: {
-        adapter: adapter({
-            fallback: '404.html'
-        }),
+        adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === 'development' ? '' : '/website'
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
         }
     }
 };
