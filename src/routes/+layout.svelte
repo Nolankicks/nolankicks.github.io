0aaadf33b8bd470ea2c1a45244c4423d111af628
Nolankicks <nolandimensions@gmail.com>
2024-10-19 16:00:56 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,11 @@
 
 <nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-blue p-2 drop-shadow-md text-white font-poppins">
     <div class="container mx-auto flex flex-row items-center justify-between">
-        <a href="/" class="hover:scale-105 transition-all active:scale-95">kicks</a>
+        <a href="" class="hover:scale-105 transition-all active:scale-95">kicks</a>
         
         <div class="flex space-x-4">
-            <a href="/projects" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Projects</a>
-            <a href="/blog" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Blogs</a>
+            <a href="projects" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Projects</a>
+            <a href="blog" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Blogs</a>
         </div>
     </div>
 </nav>
