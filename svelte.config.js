146379df69078dee80f1366318f295ef7858602b
Nolankicks <nolandimensions@gmail.com>
2024-10-19 13:36:14 -0400

    Will this build? Who the hell knows

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,44 @@
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import autoImport from 'sveltekit-autoimport';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            precompress: false,
+            strict: true,
+        }),
+        paths: {
+            base: process.env.BASE_PATH,
+        },
+        prerender: {
+            handleMissingId: ({ event, resolve }) => {
+                return;
+            }
+        }
+    },
+
+    extensions: ['.svelte', '.md', '.svx'],
     preprocess: [
         vitePreprocess(),
-
+        mdsvex(mdsvexConfig),
         autoImport({
             include: ['**/*.(svelte|md)'],
             components: ['./src/lib/components/', { name: './src' }]
         })
     ],
-
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-        
-		paths: {
-			base: process.argv.includes('dev') ? '' : "/website"
-		},
-	}
 };
 
 export default config;
