9dc38c5f394f20b9781b21e8eec62b937bdc1c61
Nolankicks <nolandimensions@gmail.com>
2024-10-19 00:16:52 -0400

    Try to fix this

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: 'build', // Output directory for static files
-            assets: 'build', // Output directory for assets
-            fallback: '404.html' // Fallback file for SPA
-        }),
-        paths: {
-            base: process.env.NODE_ENV === 'development' ? '' : '/website' // Ensure 'website' matches your repo name
-        },
-        prerender: {
-            default: true, // Enable prerendering by default
-            handleHttpError: ({ path, referrer, message }) => {
-                // Ignore 404 errors for paths that do not begin with the base path
-                if (path.startsWith('/website')) {
-                    return;
-                }
-                throw new Error(message);
-            }
-        }
-    }
+    preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : "/website"
+		}
+	}
 };
 
 export default config;
