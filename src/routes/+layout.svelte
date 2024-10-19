def0cf0829c24ddfff7edf6a7f8b061452423c7c
Nolankicks <nolandimensions@gmail.com>
2024-10-19 15:59:50 -0400

    Remove base path bull shit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,16 @@
     import Spotify from '$lib/components/Spotify.svelte';
 
     import { base } from "$app/paths";
-    
+
 </script>
 
 <nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-blue p-2 drop-shadow-md text-white font-poppins">
     <div class="container mx-auto flex flex-row items-center justify-between">
-        <a href="{base}/" class="hover:scale-105 transition-all active:scale-95">kicks</a>
+        <a href="/" class="hover:scale-105 transition-all active:scale-95">kicks</a>
         
         <div class="flex space-x-4">
-            <a href="{base}/projects" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Projects</a>
-            <a href="{base}/blog" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Blogs</a>
+            <a href="/projects" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Projects</a>
+            <a href="/blog" class="text-white hover:text-gray-300 hover:scale-105 active:text-gray-500 active:scale-95 transition-all">Blogs</a>
         </div>
     </div>
 </nav>
