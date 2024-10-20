b5f0ea8d54bf4785e2fda5675ebb23f1681cda20
Nolankicks <nolandimensions@gmail.com>
2024-10-19 23:13:34 -0400

    Update Background.svelte

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -5,7 +5,7 @@
         @apply fixed top-0 left-0 w-full h-full -z-10;
         filter: contrast(175%) brightness(100%) blur(1px);
         background:
-            linear-gradient(0deg, rgba(255, 101, 0, 0.5), transparent),
+            linear-gradient(0deg, rgba(255, 101, 0, 0.35), transparent),
             radial-gradient(circle, rgba(30, 62, 98, 0.9), transparent 300%),
             url('/noise.svg');
     }
