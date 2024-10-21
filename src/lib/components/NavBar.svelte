53bb473f74f8388a98c59ab3519313483e7b9f02
Nolankicks <nolandimensions@gmail.com>
2024-10-21 11:53:17 -0400

    Put NavBar in its own component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Link from "$lib/components/Link.svelte";
+</script>
+
+<nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-blue p-4 drop-shadow-md text-white font-poppins">
+    <div class="container mx-auto flex flex-row items-center justify-between">
+
+        <Link href="/" text="kicks" />
+        
+        <div class="flex space-x-4">
+            <Link href="/projects" text="Projects" />
+            <Link href="/blog" text="Blogs" />
+        </div>
+    </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,10 @@
     import Background from "../lib/components/Background.svelte";
     import "../syntax-highlight.css";
     import Link from "$lib/components/Link.svelte";
+	import NavBar from "$lib/components/NavBar.svelte";
 </script>
 
-<nav class="sticky top-0 z-50 mx-auto flex w-full flex-col bg-blue p-4 drop-shadow-md text-white font-poppins">
-    <div class="container mx-auto flex flex-row items-center justify-between">
-
-        <Link href="/" text="kicks" />
-        
-        <div class="flex space-x-4">
-            <Link href="/projects" text="Projects" />
-            <Link href="/blog" text="Blogs" />
-        </div>
-    </div>
-</nav>
+<NavBar />
 
 <Background />
 
