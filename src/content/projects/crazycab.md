e37940ab057c0837aa4323830772f24aa0bf6022
Nolankicks <nolandimensions@gmail.com>
2024-10-22 10:49:07 -0400

    Add caption to Video component. Add video to crazycab.md

--- src/content/projects/crazycab.md
+++ src/content/projects/crazycab.md
@@ -11,3 +11,5 @@ Crazy Cab is a fun arcade racing game. Collect the stars or race to finish, eith
 I helped work on Crazy Cab for the [Second s&box Game Jam](https://sbox.game/c/gamejam2). I worked on it with the great people at [Vidya Games](https://sbox.game/vidya).
 
 I helped with Programming and UI. I'm greatful I was able to work on it with such talented people.
+
+<Video src="crazycab/crazycab.mp4" caption="Early development footage" />
--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
-	export let src: string;
+	export let src: string = '';
+	export let caption: string = '';
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-media-has-caption -->
-<video class="mx-auto flex justify-center rounded-lg" controls>
-	<source src={"../" + src} />
-</video>
+<div class="text-center">
+	<video class="mx-auto mb-2 flex justify-center rounded-[0.3em]" controls>
+		<source src={'../' + src} />
+	</video>
+
+	{#if caption}
+		<p style="margin-top: 0px;">{caption}</p>
+	{/if}
+</div>
