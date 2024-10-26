a93acfee69aa81592d92da8901841e1522c62847
Nolankicks <nolandimensions@gmail.com>
2024-10-26 18:48:56 -0400

    Tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
     import "../app.css";
-    import { page } from '$app/stores';
-    import Background from "../lib/components/Background.svelte";
-    import "../syntax-highlight.css";
-    import Link from "$lib/components/Link.svelte";
-	import NavBar from "$lib/components/NavBar.svelte";
 </script>
-
-<NavBar />
 
 <Background />
 
-<slot></slot>
+<NavBar />
 
-
-{#if $page.url.pathname !== "/"}
-    <Footer />
-{/if}
+<div class="container mx-auto">
+	<slot />
+</div>
