14e2069a0473bb3ad49232e457a1245536dff847
Nolankicks <nolandimensions@gmail.com>
2024-10-26 18:24:20 -0400

    Absolute for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     export const pfpUrl = 'https://avatars.githubusercontent.com/u/95300917?s=400&u=4b7a75efdafbd368929511d483099250821ded21&v=4';
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center p-4 font-poppins text-white from-blue-900 via-gray-900 to-black">
+<div class="flex absolute top-0 left-0 bottom-0 right-0 flex-col items-center justify-center p-4 font-poppins text-white from-blue-900 via-gray-900 to-black">
     <div class="flex flex-col items-center justify-center gap-4 p-6 w-full max-w-xl bg-gray-800 rounded-md bg-transparentblue shadow-lg animate-fadein">
         <div class="w-full flex flex-col md:flex-row items-center">
             <img src="{pfpUrl}" class="w-40 h-40 rounded-md mb-4 md:mb-0 md:mr-6" alt="Ultra 85" />
@@ -23,7 +23,3 @@
         </div>
     </div>
 </div>
-
-<style>
-
-</style>
