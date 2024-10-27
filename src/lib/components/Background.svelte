5bba73e48edd59b72e9c6e0b59bc0dc54abddce7
Nolankicks <nolandimensions@gmail.com>
2024-10-27 14:24:38 -0400

    Fix background

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -2,7 +2,7 @@
 
 <style lang="postcss">
 	.effect {
-		@apply fixed h-screen w-screen -z-10;
+		@apply fixed top-0 left-0 w-full h-full -z-10;
 		filter: contrast(150%) blur(1px);
 		background: linear-gradient(0deg, rgba(255, 101, 0, 0.36), transparent 100%),
 			linear-gradient(0deg, rgba(30, 62, 98, 0.75), transparent 400%), url('/noise.svg');
