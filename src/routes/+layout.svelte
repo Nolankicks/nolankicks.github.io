bb9433c9840f14d6e28208826cd64a8ebdba9c13
Nolankicks <nolandimensions@gmail.com>
2024-10-26 21:07:11 -0400

    Revert "Tweaks"
    This reverts commit a93acfee69aa81592d92da8901841e1522c62847.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
     import "../app.css";
+    import { page } from '$app/stores';
+    import Background from "../lib/components/Background.svelte";
+    import "../syntax-highlight.css";
+    import Link from "$lib/components/Link.svelte";
+	import NavBar from "$lib/components/NavBar.svelte";
 </script>
-
-<Background />
 
 <NavBar />
 
-<div class="container mx-auto">
-	<slot />
-</div>
+<Background />
+
+<slot></slot>
+
+
+{#if $page.url.pathname !== "/"}
+    <Footer />
+{/if}
