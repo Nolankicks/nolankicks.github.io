8cd37a048a1d8a4b9510c99841d425d6ddf7e601
Nolankicks <nolandimensions@gmail.com>
2024-10-29 16:10:31 -0400

    Add hyper links on headers

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
 	export let title: string;
-    export let author: string | undefined;
-    export let authorLink: string | undefined;
+	export let author: string | undefined;
+	export let authorLink: string | undefined;
 </script>
 
-<div>
-	<p class="font-poppins font-bold text-5xl mb-0 mt-0 text-kindagrey">{title}</p>
-	<hr class="mt-3 {authorLink ? "mb-0" : "mb-4"}" />
+<svelte:element this="h" id={title}>
+	<div>
+		<a href={`#${title}`} target="_self" style="text-decoration: none;">
+			<p
+				class="font-poppins font-bold text-5xl mb-0 mt-0 text-kindagrey hover:text-orange transition-all"
+			>
+				{title}
+			</p>
+		</a>
+		<hr class="mt-3 {authorLink ? 'mb-0' : 'mb-4'}" />
 
-    {#if author}
-        {#if authorLink}
-            <p class="text-2xl font-poppins">By <a href={authorLink}>{author}</a></p>
-        {/if}
-    
-        {#if !authorLink}
-            <p class="text-2xl font-poppins">By {author}</p>
-        {/if}
-    {/if}
+		{#if author}
+			{#if authorLink}
+				<p class="text-2xl font-poppins">
+					By <a href={authorLink} style="text-decoration: none;">{author}</a>
+				</p>
+			{/if}
 
-</div>
+			{#if !authorLink}
+				<p class="text-2xl font-poppins">By {author}</p>
+			{/if}
+		{/if}
+	</div>
+</svelte:element>
