4b1b36101240ff7990fb9025c101ae9b8d38d3a8
Kicks <nolandimensions@gmail.com>
2024-11-11 21:30:15 -0500

    Oops, forgot sbox blog page

--- src/routes/sboxnews/[slug]/+page.svelte
+++ src/routes/sboxnews/[slug]/+page.svelte
@@ -32,8 +32,8 @@
 <article class="flex flex-col font-poppins w-full mx-auto 2xl:w-[75%]">
         <div class="mb-10 text-white text-shadow flex flex-col self-center">
         <hr class="border-t-2 mb-10 text-white mx-4" />
-        <article class="bg-transparentblue rounded-md p-5 font-poppins bg-blackalpha backdrop-blur-3xl shadow-2xl justify-center flex animate-fadein mx-4">
-            <article class="prose text-white prose-headings:font-bold prose-headings:text-white prose-headings:font-poppins prose-a:underline prose-a:text-white hover:prose-a:text-altblue hover:prose-a:transition-all">
+        <article class="bg-secondary rounded-md p-5 font-poppins bg-blackalpha backdrop-blur-3xl shadow-2xl justify-center flex animate-fadein mx-4">
+            <article class="prose text-white prose-headings:font-bold prose-headings:text-white prose-headings:font-poppins prose-a:underline prose-a:text-white hover:prose-a:text-tertiary hover:prose-a:transition-all">
                 <div class="flex w-full h-12 mb-4 gap-4">
                     <a href="https://sbox.game{data.post?.Url}" class="flex items-center justify-center">
                         <img src="../sbox.png" alt="An icon for a link" class="rounded-md bg-cover h-12 w-12 hover:scale-105 active:scale-95 transition-all" />
