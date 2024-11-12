cb5fb67d50d52baf2b3d07d3701706fd22efdf7c
Kicks <nolandimensions@gmail.com>
2024-11-11 22:00:05 -0500

    Formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,19 @@
 <script>
     export const pfpUrl = 'https://avatars.githubusercontent.com/u/95300917?s=400&u=4b7a75efdafbd368929511d483099250821ded21&v=4';
-  </script>
+</script>
   
-  <svelte:head>
+<svelte:head>
     <title>Kicks</title>
-  </svelte:head>
+</svelte:head>
   
-  <!-- Parent container -->
-  <div class="flex absolute inset-0 flex-col items-center justify-center font-poppins text-white bg-gradient-to-b from-blue-900 via-gray-900 to-black">
-  
-    <!-- Main content container -->
+<div class="flex absolute inset-0 flex-col items-center justify-center font-poppins text-white bg-gradient-to-b from-blue-900 via-gray-900 to-black">
     <div class="flex flex-col items-center justify-center gap-4 w-full max-w-xl rounded-md animate-fadein">
-      
-      <!-- Text content -->
       <div class="w-full flex flex-col md:flex-row items-center text-center md:text-left">
         <div class="flex flex-col items-center px-5">
           <div class="text-3xl md:text-4xl font-bold mb-2 w-full">Hey, I'm Kicks</div>
           <div class="text-xl md:text-2xl">I enjoy programming using C#, TypeScript/JavaScript, and HTML/CSS</div>
         </div>
       </div>
-  
-      <!-- Social media links -->
       <div class="flex flex-row justify-center md:justify-start w-full gap-4 px-5">
         <a href="https://github.com/Nolankicks">
           <img src="github-mark.png" alt="GitHub" class="bg-cover h-12 w-12 hover:scale-105 active:scale-95 transition-all" />
@@ -30,7 +23,7 @@
         </a>
       </div>
     </div>
-  </div>
+</div>
   
-  <DiscordEmbed title="Kicks" description="My personal website" />
+<DiscordEmbed title="Kicks" description="My personal website" />
   
