5001dd9230cf55cd4b0e73fd8b40ac6f67ab9606
Kicks <nolandimensions@gmail.com>
2024-11-11 21:58:50 -0500

    Fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,36 @@
 <script>
     export const pfpUrl = 'https://avatars.githubusercontent.com/u/95300917?s=400&u=4b7a75efdafbd368929511d483099250821ded21&v=4';
-</script>
-
-<svelte:head>
+  </script>
+  
+  <svelte:head>
     <title>Kicks</title>
-</svelte:head>
-
-<div class="flex absolute top-0 left-0 bottom-0 right-0 flex-col items-center justify-center font-poppins text-white from-blue-900 via-gray-900 to-black">
+  </svelte:head>
+  
+  <!-- Parent container -->
+  <div class="flex absolute inset-0 flex-col items-center justify-center font-poppins text-white bg-gradient-to-b from-blue-900 via-gray-900 to-black">
+  
+    <!-- Main content container -->
     <div class="flex flex-col items-center justify-center gap-4 w-full max-w-xl rounded-md animate-fadein">
-        <div class="w-full flex flex-col md:flex-row items-center">
-            <div class="flex flex-col items-center md:items-start text-center md:text-left">
-                <div class="text-3xl md:text-4xl font-bold mb-2">Hey, I'm kicks</div>
-                <div class="text-xl md:text-2xl">I enjoy programming using C#, TypeScript/JavaScript, and HTML + CSS</div>
-            </div>
+      
+      <!-- Text content -->
+      <div class="w-full flex flex-col md:flex-row items-center text-center md:text-left">
+        <div class="flex flex-col items-center px-5">
+          <div class="text-3xl md:text-4xl font-bold mb-2 w-full">Hey, I'm Kicks</div>
+          <div class="text-xl md:text-2xl">I enjoy programming using C#, TypeScript/JavaScript, and HTML/CSS</div>
         </div>
-        <div class="flex flex-row md:justify-start justify-center w-full gap-4">
-            <a href="https://github.com/Nolankicks">
-                <img src="github-mark.png" alt="An icon for the website github" class="bg-cover h-12 w-12 hover:scale-105 active:scale-95 transition-all" />
-            </a>
-            
-            <a href="https://sbox.game/nolankicks">
-                <img src="sbox.png" alt="An icon for the game s&box" class="bg-cover rounded-md h-12 w-12 hover:scale-105 active:scale-95 transition-all" />
-            </a>
-        </div>
+      </div>
+  
+      <!-- Social media links -->
+      <div class="flex flex-row justify-center md:justify-start w-full gap-4 px-5">
+        <a href="https://github.com/Nolankicks">
+          <img src="github-mark.png" alt="GitHub" class="bg-cover h-12 w-12 hover:scale-105 active:scale-95 transition-all" />
+        </a>
+        <a href="https://sbox.game/nolankicks">
+          <img src="sbox.png" alt="S&Box" class="bg-cover rounded-md h-12 w-12 hover:scale-105 active:scale-95 transition-all" />
+        </a>
+      </div>
     </div>
-</div>
-
-<DiscordEmbed title="Kicks" description="My personal website" />
+  </div>
+  
+  <DiscordEmbed title="Kicks" description="My personal website" />
+  
