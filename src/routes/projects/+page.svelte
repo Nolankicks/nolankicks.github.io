744aea08f2bf7a36f1027f24bcedec8e08bce867
Kicks <nolandimensions@gmail.com>
2024-11-16 19:58:51 -0500

    Fix projects title

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,7 +11,7 @@
 
 <div class="w-full flex items-center justify-center animate-fadein">
     <div class="max-w-xl w-full my-5 mx-auto flex flex-col gap-4 pt-6 font-poppins px-5 text-white sm:px-0">
-        <Header title="Blogs" HyperLink={false} />
+        <Header title="Projects" HyperLink={false} />
     </div>
 </div>
 
