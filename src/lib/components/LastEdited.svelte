3681674cd50bc7ea3ef82fe257237c3f8e50aff6
Kicks <nolandimensions@gmail.com>
2024-11-28 17:41:59 -0500

    Fix date

--- src/lib/components/LastEdited.svelte
+++ src/lib/components/LastEdited.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
     export let date = '';
-    export let text = '';
 </script>
 
-<p class="text-sm text-center w-full flex">{text} {new Date(date).toLocaleString('en-us', {
-    month: 'short',
-    year: 'numeric'
-})}</p>
+<p class="text-sm text-center w-full flex">{date}</p>
